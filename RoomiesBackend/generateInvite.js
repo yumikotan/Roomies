6c9bd72549c72a4e290ad793b1f02076e5339e97
Safeeyah Hoang <safeeyahhoang@gmail.com>
2025-11-14 22:57:01 -0800

    delete duplicate files

--- RoomiesBackend/generateInvite.js
+++
@@ -1,8 +0,0 @@
-// generates a new invite code dynamically
-
-export function generateInviteCode() {
-  const inviteCode = Math.random().toString(36).substring(2, 8).toUpperCase();
-  console.log('New invite code generated:', inviteCode);
-  
-  return inviteCode;
-}
